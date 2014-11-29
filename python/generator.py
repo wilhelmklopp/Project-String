10d828eb0280c30316e701d4cf219c2bc8cf454d
Wilhelm Klopp <wil.klopp@gmail.com>
2014-11-29 23:44:53 +0000

    Added generator and resolve functions

--- python/generator.py
+++ python/generator.py
@@ -1,3 +1,6 @@
-generate(long_url):
-    #long_url is user input
+generate(iv):
+    #iv = index value in database
     alphabet = ["b","c","d","f","g","h","j","k","m","n","p","q","r","s","t","v","w","x","y","z","B","C","D","F","G","H","J","K","M","N","P","Q","R","S","T","V","W","X","Y","3","4","5","6","7","8","9"]
+    short = []
+    short.append(alphabet[iv%len(alphabet)])
+    print (short)
