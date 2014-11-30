b11c024de6ded5dcbb37fcc7e0ecb65dc6dea3d2
Wilhelm Klopp <wil.klopp@gmail.com>
2014-11-30 21:13:33 +0000

    Extended testing by displaying short variable.

--- python/testing.py
+++ python/testing.py
@@ -1,9 +1,9 @@
 from generator import generate
 from generator import resolve
 
-for i in xrange(1, 999999999):
+for i in xrange(53833, 999999999):
     short = generate(i)
     if i == resolve(short):
-        print("Pass: " , i)
+        print("Pass: " , i, short)
     else:
-        print("Error with: ", i)
+        print("Error with: ", i, short)
