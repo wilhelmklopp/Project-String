20b9fdca3ae8d41e5d20ea9b97f7bdf6c6893a27
Wilhelm Klopp <wil.klopp@gmail.com>
2014-11-30 00:00:04 +0000

    Reverted commit: only generator was added. File was not fully staged.
    This reverts commit 10d828eb0280c30316e701d4cf219c2bc8cf454d.

--- python/generator.py
+++ python/generator.py
@@ -1,6 +1,3 @@
-generate(iv):
-    #iv = index value in database
+generate(long_url):
+    #long_url is user input
     alphabet = ["b","c","d","f","g","h","j","k","m","n","p","q","r","s","t","v","w","x","y","z","B","C","D","F","G","H","J","K","M","N","P","Q","R","S","T","V","W","X","Y","3","4","5","6","7","8","9"]
-    short = []
-    short.append(alphabet[iv%len(alphabet)])
-    print (short)
