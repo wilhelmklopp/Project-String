b30c3bde8c88dd831f38560432aba6d94ddadfdb
Wilhelm Klopp <wil.klopp@gmail.com>
2014-12-01 11:06:53 +0000

    Added top comment for improved clarity.

--- python/testing.py
+++ python/testing.py
@@ -1,7 +1,8 @@
+# tests validity of each generated short
 from generator import generate
 from generator import resolve
 
-for i in xrange(53833, 999999999):
+for i in xrange(1997000, 999999999):
     short = generate(i)
     if i == resolve(short):
         print("Pass: " , i, short)
