e1115b57482a93da4386a733cf8792c608756049
Wilhelm Klopp <wil.klopp@gmail.com>
2014-12-02 22:50:58 +0000

    Completed testing up to Index Value 10mil

--- python/testing.py
+++ python/testing.py
@@ -2,9 +2,10 @@
 from generator import generate
 from generator import resolve
 
-for i in xrange(1997000, 999999999):
+for i in xrange(10000000, 999999999):
     short = generate(i)
     if i == resolve(short):
         print("Pass: " , i, short)
     else:
-        print("Error with: ", i, short)
+        print("Error with: ", i, short, "--------")
+#tested up to 10 million
