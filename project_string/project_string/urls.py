91ab85834abbc61f381a3326f93f40f5b1b38540
Wilhelm Klopp <wil.klopp@gmail.com>
2015-02-20 19:33:03 +0000

    removed legacy comments in urls.py

--- project_string/project_string/urls.py
+++ project_string/project_string/urls.py
@@ -3,9 +3,6 @@ from django.contrib import admin
 from pstring import views
 
 urlpatterns = patterns('',
-    #url(r'^pstring/', include('pstring.urls')),
-    #url(r'^admin/', include(admin.site.urls)),
-    #url(r'$', include('pstring.urls')),
     url(r'^$', include('pstring.urls')),
     url(r'^(?P<short>[-bcdfghjkmnpqrstvwxyzBCDFGHJKMNPQRSTVWXY3456789]+)/$', views.resolve_landing, name='resolve_landing'),
     url(r'^generate/$', views.show_form, name='generate'),                   
